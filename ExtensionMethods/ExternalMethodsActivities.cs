46b26e32132a37804c2faabf7e4b9a6804a6c9d1
Przem1998 <p.zeleznicki@protonmail.com>
2020-04-19 02:48:29 +0200

    Add option upper even letters and lower odd one

---
+++ ExtensionMethods/ExternalMethodsActivities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    static class ExternalMethodsActivities
+    {
+        public static string UpperEvenLetter(this string s)
+        {
+            string TempS = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 2 == 0) TempS += char.ToUpper(s[i]);
+                else TempS += char.ToLower(s[i]);
+            }
+
+            return TempS;
+        }
+        
+    }
+}
---
+++ ExtensionMethods/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExtensionMethods
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _Word = "";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TransformString_Click(object sender, RoutedEventArgs e)
+        {
+
+            _Word = inputWord.Text;
+            if (_Word.Length == 0)
+            {
+                MessageBox.Show("Nie wprowadzono napisu");
+                afterTransform.Content = "Błąd!!!";
+                afterTransform.Foreground = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                afterTransform.Foreground = new SolidColorBrush(Colors.Black);
+                if (UpperLowerString.IsChecked == true) afterTransform.Content = "Przekształcony napis: " + _Word.UpperEvenLetter();
+                
+
+            }
+        }
+
+    }
+}
