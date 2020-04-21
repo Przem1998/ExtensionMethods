f0141c20a1f0ed229f289bdb0e61b838ce06d72e
Przem1998 <p.zeleznicki@protonmail.com>
2020-04-21 11:01:30 +0200

    add option counting amount of words in inscription

--- ExtensionMethods/ExternalMethodsActivities.cs
+++ ExtensionMethods/ExternalMethodsActivities.cs
@@ -28,6 +28,66 @@ namespace ExtensionMethods
         {
             return Regex.Replace(s, "[aeyioąęuóAEYIOĄĘUÓ]", "");
         }
+        public static int CountWordsInString(this string s)
+        {
+            var words = s.Split(' ');
+            int w = 0;
+            foreach (string ws in words)
+            {
+                if (IsWord(ws)) w++;
+            }
+            return w;
+        }
+        private static bool IsWord(string s)
+        {
+            int l = 0; //count  leters/punctuation chars
+            int p = 0; //count points 
+            int tempIndex = 0; //use to compare with index in array to aims check countains multpoint
+            char[] punctuations = { '.', '-', ',', '!', '?', ';' };
+            char[] brackets = { '(', '[', '"' };
+            if (s.Length == 1)
+            {
+                char c = s[0];
+                if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) return true;
+            }
 
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))
+                {
+                    l++;
+                    if (l == s.Length - 1) return true;
+                }
+                else
+                {
+                    if (l == s.Length - 1)
+                    {
+                        if (s[i].ToString().Contains(s[i])) return true;
+                    }
+                    else if (s[i] == 46 && l > 0)
+                    {
+                        if (p == 0)
+                        {
+                            p++;
+                            tempIndex = i;
+                        }
+                        else if (i - tempIndex == 1)
+                        {
+                            p++;
+                            tempIndex = i;
+                            if (p == 3) return true;
+                        }
+
+                        else return false;
+                    }
+                    else
+                        return false;
+                }
+            }
+            return false;
+        }
+
+    
     }
+}
 }
